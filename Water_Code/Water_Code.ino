7c1723b625eea1dc67b430ec80127cf0a0271f86
Andrew Scott <69655225+ajscott0@users.noreply.github.com>
2023-11-21 14:14:26 -0500

    Mission accomplishing functions entered

--- Water_Code/Water_Code.ino
+++ Water_Code/Water_Code.ino
@@ -3,7 +3,7 @@
 
 void setup() {
   // Team Name, Mission Type, Marker ID, Wifi Module RX Pin, Wifi Module TX Pin
-  Enes100.begin("Ban-anna Mike-anics", WATER, 19, 10, 11);
+  Enes100.begin("Ban-anna Mike-anics", WATER, 209, 11, 10);
   Enes100.println("Connected...");
 
   pinMode(AtrigPin, OUTPUT);
@@ -31,49 +31,20 @@ void setup() {
 
 void loop() {
 
-  // if (v) {  // If the ArUco marker is visible
-  //   Enes100.print(x); // print out the location
-  //   Enes100.print(",");
-  //   Enes100.print(y);
-  //   Enes100.print(",");
-  //   Enes100.println(t);
-  // } else {
-  //   Enes100.println("Not visible");
-  // }
-
-  /* Navigation Section */
-  forward();
-  delay(1000);
-  stop();
-  delay(1000);
-
-  
-  // **Navigation Section**
-  // if (distance_sensor < 20){    //Sense obstacle (20cm) *can change the distance to whatever*
-  //     Serial.println("stop");
-  //     stop(); // stop (3 seconds)
-  //     //We can add a "while (sum > ___) here
-  //     //Or just do while instead of if
-  // } else { //No obstacle in front
-  //     Serial.println("forward");
-  //     forward();
-  // }
-
-  
-  
-
-  
-  // Transmit the state of the pool
-  if (tds_go() && turbidity_go()) {
-    Enes100.mission(WATER_TYPE, SALT_POLLUTED);
-  } else if (tds_go() && !turbidity_go()) {
-    Enes100.mission(WATER_TYPE, SALT_UNPOLLUTED);
-  } else if (!tds_go() && turbidity_go()) {
-    Enes100.mission(WATER_TYPE, FRESH_POLLUTED);
-  } else if (!tds_go() && !turbidity_go()) {
-    Enes100.mission(WATER_TYPE, FRESH_UNPOLLUTED);
+  if (v) {  // If the ArUco marker is visible
+    Enes100.print(x); // print out the location
+    Enes100.print(",");
+    Enes100.print(y);
+    Enes100.print(",");
+    Enes100.println(t);
+  } else {
+    Enes100.println("Not visible");
   }
 
-  // Transmit the depth of the pool in mm (20, 30, or 40)
-  Enes100.mission(DEPTH, depth());
+  /* Navigation Section */
+  get_to_site();
+  collect_water();
+  obtain_data();
+  get_to_destination();
+
 }
