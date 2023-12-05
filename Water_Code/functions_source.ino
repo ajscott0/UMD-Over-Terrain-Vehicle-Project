ee8e003c7d94c71309f237effaa068efd8f2679a
Andrew Scott <69655225+ajscott0@users.noreply.github.com>
2023-12-04 20:19:35 -0500

    Results of monday work

--- Water_Code/functions_source.ino
+++ Water_Code/functions_source.ino
@@ -38,7 +38,8 @@ void left_turn() {
 
   digitalWrite(leftFrontPin1, HIGH);
   digitalWrite(leftFrontPin2, LOW);
-  digitalWrite(leftRearPin1, HIGH);;
+  digitalWrite(leftRearPin1, HIGH);
+  ;
   digitalWrite(leftRearPin2, LOW);
 }
 
@@ -68,16 +69,18 @@ void stop() {
 
 // Sensor function implementations
 float distance1(int trigPin, int echoPin) {
-  digitalWrite(trigPin, LOW); // Pulsing trigger pin
+  float duration, distance_in_func;
+
+  digitalWrite(trigPin, LOW);  // Pulsing trigger pin
   delayMicroseconds(2);
   digitalWrite(trigPin, HIGH);
   delayMicroseconds(10);
   digitalWrite(trigPin, LOW);
 
-  long duration = pulseIn(echoPin, HIGH); // Measure time taken for echo pin to go HIGH
-  int distance = (duration / 29) / 2;  // Speed of sound = 29 microsec/cm. Divided by 2 b/c back and forth.
+  duration = pulseIn(echoPin, HIGH);  // Measure time taken for echo pin to go HIGH
+  distance_in_func = (duration * 0.0343) / 2;      // Speed of sound = 29 microsec/cm. Divided by 2 b/c back and forth.
 
-  return distance;
+  return distance_in_func;
 }
 
 int tds_go() {
@@ -105,7 +108,7 @@ int turbidity_go() {
 
 float depth() {
   float dist_to_water = distance1(BtrigPin, BechoPin);
-  float depth = ARM_HEIGHT - POOL_THICK - 3 - (dist_to_water * 10); // - 3 from Velcro height
+  float depth = ARM_HEIGHT - POOL_THICK - 3 - (dist_to_water * 10);  // - 3 from Velcro height
 
   return depth;
 }
@@ -133,7 +136,6 @@ void turn_to(double target_angle) {
     } else {
       if ((t - target_angle) < 0) {
         left_turn();
-        Enes100.println(t - target_angle);
       } else {
         right_turn();
       }
@@ -155,22 +157,20 @@ int get_to_point(double target_x, double target_y) {
   distance = distance1(AtrigPin, AechoPin);
 
   heading = atan2(target_y - cur_y, target_x - cur_x);
-  Enes100.println(heading);
-  Enes100.println(distance);
 
   turn_to(heading);
 
   bool keep_driving = true;
   unsigned long current_time;
   unsigned long last_recalculation = 0;
-  unsigned long recalculation_interval = 2000;
+  unsigned long recalculation_interval = 1500;
 
   while (keep_driving == true) {
     Enes100.updateLocation();
     cur_x = Enes100.location.x;
     cur_y = Enes100.location.y;
     distance = distance1(AtrigPin, AechoPin);
-    Enes100.println("Distance: ");
+    Enes100.print("Distance: ");
     Enes100.println(distance);
 
     if (cur_x > (target_x - tolerance) && cur_x < (target_x + tolerance) && cur_y > (target_y - tolerance) && cur_y < (target_y + tolerance)) {
@@ -178,7 +178,7 @@ int get_to_point(double target_x, double target_y) {
       keep_driving = false;
       return 1;
     }
-    if (distance < 30) {
+    if (distance < 11) {
       stop();
       keep_driving = false;
       return 2;
@@ -229,7 +229,7 @@ int obtain_data() {
 
   // Transmit the depth of the pool in mm (20, 30, or 40)
   Enes100.mission(DEPTH, depth());
-  
+
   return 1;
 }
 
@@ -240,26 +240,54 @@ int get_to_destination() {
   cur_y = Enes100.location.y;
   distance = distance1(AtrigPin, AechoPin);
 
-  if (cur_y > 1) {  // If on top
-    if (get_to_point(1.5, 1.5) == 1) {  // Got to point
-      get_to_point(1.9, 1.5);
-    } else if (get_to_point(1.5, 1.5) == 2) { // Encountered obstacle
-      get_to_point(1.5, 1.8);
+  if (cur_y > 1) {           // If on top
+    get_to_point(1.1, 1.5);  // Approach potential obstacle
+    Enes100.println("Function executed");
+    if (get_to_point(1.5, 1.5) == 1) {    // Got to point
+      get_to_point(1.9, 1.5);             // Continue
+    } else {                              // Encountered obstacle
+      get_to_point(1.1, 1.0);             // Approach potential obstacle
+      if (get_to_point(1.5, 1.0) == 1) {  // Got to point
+        get_to_point(1.9, 1.0);           // continue
+      } else {                            // Encountered obstacle
+        get_to_point(1.1, 0.5);           // Approach known NO obstacle
+        get_to_point(1.5, 0.5);           // Got to point
+        get_to_point(1.9, 0.5);           // continue
+      }
     }
-  } else if (y <= 1) {  // If on bottom
-    if (get_to_point(1.5, 0.5) == 1) {  // Got to point
-      get_to_point(1.9, 0.5);
-    } else if (get_to_point(1.5, 0.5) == 2) { // Encountered obstacle
-      get_to_point(1.5, 1.8);
+  } else if (y <= 1) {                    // If on bottom
+    get_to_point(1.1, 0.5);               // Aproach potential obstalce
+    if (get_to_point(1.5, 0.5) == 1) {    // Got to point
+      get_to_point(1.9, 0.5);             // Continue
+    } else {                              // Encountered obstacle
+      get_to_point(1.1, 1.0);             // Approach potential obtacle
+      if (get_to_point(1.5, 1.0) == 1) {  // Got to point
+        get_to_point(1.9, 1.0);           // continue
+      } else {                            // Encountered obstalce
+        get_to_point(1.1, 1.5);           // Approach known NO obstacle
+        get_to_point(1.5, 1.5);           // Go to point
+        get_to_point(1.9, 1.5);           // Continue
+      }
     }
   }
 
-  get_to_point(1.9, 1.0); // Go to middle either way
+  get_to_point(1.9, 1.5);  // Start trying from top either way (closer to limbo)
 
-  
+  if (get_to_point(2.3, 1.5) == 1) {    // Got to point
+    get_to_point(3.1, 1.5);             // continue
+  } else {                              // Encountered obstacle
+    get_to_point(1.9, 1.0);             // Appraoch potential obstalce
+    if (get_to_point(2.3, 1.0) == 1) {  // Got to point
+      get_to_point(3.1, 1.0);           // continue
+    } else {                            // Encountered obstacle
+      get_to_point(1.9, 0.5);           // Approach known NO obstacle
+      get_to_point(2.3, 0.5);           // Go to point
+      get_to_point(3.1, 0.5);           // continue
+    }
+  }
 
-  // Go up to limbo, if not already there
-  // Go under limbo
+  get_to_point(3.1, 1.5);  // Go to front of limbo in perperation from going under
+  get_to_point(3.7, 1.5);  // Go under limbo
 
   return 1;
 }
